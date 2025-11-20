d90e5a650fc102ca63b455fb674bc4fc0df13d55
yxy <yxy3213493159@qq.com>
2025-11-20 20:45:20 +0800

    下班

--- src/supabase.js
+++ src/supabase.js
@@ -1,13 +1,18 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.SUPABASE_URL
-const supabaseKey = import.meta.env.SUPABASE_ANON_KEY
-
+console.log('import.meta.env', import.meta.env)
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey =
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+console.log('supabaseUrl', supabaseUrl)
+console.log('supabaseKey', supabaseKey)
 if (!supabaseUrl || !supabaseKey) {
-  const errorMsg = 'Supabase 环境变量未配置，请在 .env 中设置 SUPABASE_URL 和 SUPABASE_ANON_KEY'
+  const errorMsg =
+    'Supabase 环境变量未配置，请在 .env 中设置 VITE_SUPABASE_URL 与 VITE_SUPABASE_ANON_KEY'
   // eslint-disable-next-line no-console
   console.error(errorMsg)
   throw new Error(errorMsg)
+  
 }
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
